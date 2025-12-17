bdc79e2b5e46b863d0c75179ab2f4c01cb09e533
neline388 <negarlaleh1@gmail.com>
2025-12-17 14:29:45 +0100

    error

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -53,8 +53,10 @@ const HeroImage = () => {
   if (isMobile) {
     return (
       <div className="hero-image mobile">
-        <img src="/images/camera.png" alt="Camera" />
-      </div>
+<img
+  src={`${import.meta.env.BASE_URL}images/camera.png`}
+  alt="Camera"
+/>      </div>
     );
   }
 
